f870252fa4d171625b0497724688b8f5a5023280
Hussein <hgomaa0@gmail.com>
2026-01-04 15:59:49 +0200

    fix: polyfill AUTH_SECRET to guarantee environment variable existence

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,8 @@ import bcrypt from "bcryptjs"
 import { z } from "zod"
 import { authConfig } from "./auth.config"
 
+if (!process.env.AUTH_SECRET) process.env.AUTH_SECRET = "7f8a9b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z"
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
     ...authConfig,
     adapter: PrismaAdapter(prisma) as any,
